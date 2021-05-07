2678a6af51763a179df7dd952554c3ba297b016f
ronaldcorradi@hotmail.com <ronaldcorradi@hotmail.com>
2021-05-07 19:55:30 -0300

    Project back-end created

---
+++ backend/dsvendas/src/test/java/br/com/aplicacao/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aplicacao.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
