98874dee99197baef2385a127c79f9dc3e9b7f3c
ronaldcorradi@hotmail.com <ronaldcorradi@hotmail.com>
2021-05-08 18:15:34 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,38 +1,36 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
 type ChartData = {
-    labels:string[],
-    series:number[]
+    labels: string[],
+    series: number[]
 }
 
 const DonutChart = () => {
 
-    let chartData : ChartData = {labels : [], series : []};
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-         .then(response=>{
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(sale=>sale.sellerName);
-            const mySeries = data.map(sale=>sale.sum);
-            chartData = {labels : myLabels, series : mySeries};
-            console.log(response.data);
-         });
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-   // }
-    
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(sale => sale.sellerName);
+                const mySeries = data.map(sale => sale.sum);
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, [])
+
     const options = {
         legend: {
             show: true
         }
     }
-    return(    
+    return (
         <Chart
-            options={{...options,labels:chartData.labels}}
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
