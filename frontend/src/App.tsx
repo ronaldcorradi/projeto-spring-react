ab98aefc4648cb783a257587c7f6d8d23d56379a
ronaldcorradi@hotmail.com <ronaldcorradi@hotmail.com>
2021-05-07 22:25:58 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import DataTable from 'components/DataTable/index'
+import Footer from 'components/Footer/index'
+import NavBar from 'components/NavBar/index'
+
 function App() {
   return (
-    <div>
+    <>
+    <NavBar/>
+    <div className="container">
       <h1 className="text-primary">Hello World</h1>
     </div>
+    <div>
+      <DataTable/>
+    </div>
+    <Footer/>
+    </>
   );
 }
 
